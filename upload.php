0291467e96c2f8abb664b50477ba21c59dfef98c
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2018-05-01 10:10:58 -0400

    Fixes SBU-BMI/quip_distro#243

--- upload.php
+++ upload.php
@@ -7,7 +7,7 @@
     <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <!--Import materialize.css-->
     <link type="text/css" rel="stylesheet" href="materialize/css/materialize.min.css"  media="screen,projection"/>
-    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script type="text/javascript" src="../js/jquery-3.3.1.min.js"></script>
     <script type="text/javascript" src="materialize/js/materialize.min.js"></script>
     <script src="js/jquery.form.js"></script>
     <script type="text/javascript" src="materialize/js/materialize.min.js"></script>
