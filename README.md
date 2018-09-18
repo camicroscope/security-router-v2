3c7c12ec7466d2fcef421123e5df97c3527f1969
Ryan Birmingham <birm@rbirm.us>
2018-09-18 15:52:55 -0400

    partiall tested routing

--- README.md
+++ README.md
@@ -1,10 +1,26 @@
 # caMicroscope Security and Router Container
 TODO
 
-## routes.json
+## Configuration
+
+routes.json
 
 ### Variables
 {IN}
 {OUT}
 {USER}
 {NOW}
+
+### Resolvers
+source - the pattern to ger {IN} from
+destination - what to use as the method url, after {OUT} substitution,
+url - the url to check
+field - the field in the response to assign to {OUT}
+before - a string or list of strings to get the variable before; if multiple match, the first match is used
+after - a string or list of strings to get the variable after; if multiple match, the first match is used
+
+### Keycheck
+!!!! Keycheck is not yet implemented, but part of a potential concept for later
+
+Checks if a named field is present in both the users permissions and the document
+If passIfMissing is truthy, it will return a document with no such field
--- router.js
+++ router.js
@@ -1,4 +1,4 @@
-// This tool routes in three steps
+
 // intended to be used (at least) as an exposed docker container
 const express = require('express')
 const rp = require('request-promise');
@@ -8,34 +8,90 @@ const getUrlParam = require("./getUrlParam")
 
 app.use(express.json());
 
+let loading_config
+try{
+  loading_config = JSON.parse(fs.readFileSync("routes.json"));
+} catch (e){
+  loading_config = JSON.parse(fs.readFileSync("routes.json.example"));
+}
+const config = loading_config;
 
 // Sessions
 // CACHE Resolvers
 // CACHE Keys, put in session
 
+// route for user - get username given auth header
+// route for login - get header given good login
+
+// this method takes in the original url and config to resolve which url to ask for
+async function resolve(url, config){
+  let service = url.split("/")[1]
+  let type = url.split("/")[2]
+  let method = url.split("/")[3]
+  console.log(method)
+  let outUrl = ""
+  // check if exists first
+  let serviceList = config.services
+  let hasMethod = serviceList.hasOwnProperty(service)
+    && serviceList[service].hasOwnProperty(type)
+    && serviceList[service][type].hasOwnProperty(method);
+  if (hasMethod){
+    outUrl+= serviceList[service]["_base"] || ""
+    if (!(typeof serviceList[service][type][method] === 'string' || serviceList[service][type][method] instanceof String)){
+      outUrl += await useResolver(method, serviceList[service][type][method])
+    } else {
+      outUrl += serviceList[service][type][method] || ""
+    }
+  } else {
+    // if not exists, go to base
+    outUrl = config["root"]+"/" + url.split("/").slice(1).join("/")
+  }
+  return outUrl
+}
+
+// in cases where a resolver, rather than a string, is used for a method, use this to lookup w/o cache
+async function useResolver(method, rule){
+  var INvar = method;
+  var beforeVar = "";
+  var afterVar = "";
+  // get input variable
+  if (rule.before){
+    if (!(typeof rule.before === 'string' || rule.before instanceof String)){
+      // for unity, treat as list
+      rule.before = [rule.before]
+    }
+    let activeKeys = rule.before.filter(x=>INvar.indexOf(x)>=0)
+    INvar = INvar.split(activeKeys[0])[0]
+    // keep the rest of the things surrounding the invar
+    beforeVar = method.split(activeKeys[0]).slice(1).join(activeKeys[0])
+  }
+  if (rule.after){
+    if (!(typeof rule.after === 'string' || rule.after instanceof String)){
+      // for unity, treat as list
+      rule.after = [rule.after]
+    }
+    let activeKeys = rule.after.filter(x=>INvar.indexOf(x)>=0)
+    INvar = INvar.split(activeKeys[0])[1]
+    // keep the rest of the things surrounding the invar
+    afterVar = method.split(activeKeys[0]).slice(0,-1).join(activeKeys[0])
+  }
+  // TODO ask cache for this invar
+  var OUTvar = await rp({uri:rule.url.split("{IN}").join(INvar), json:true})
+  if (rule.field){
+    OUTvar = OUTvar[rule.field]
+  }
+  // substitute all OUT and IN
+  return rule.destination.split("{OUT}").join(beforeVar + OUTvar + afterVar).split("{IN}").join(INvar);
+}
+
 // User/security routes
 
-// resolve using routes.json
 
 
 app.use("/", async function(req, res){
-  let type = req.originalUrl.split("/")[1]
-  let path = req.originalUrl.split("/").slice(2).join("/");
-  let auth = req.headers.authorization;
-  // check auth
-  var is_authorized
-  try{
-    is_authorized = await checkAuth(type, path, auth, req)
-  } catch(e){
-    console.warn(e)
-    res.status(500).send(decodeURIComponent(e))
-  }
-  // skip this check if told to
-  if (!is_authorized) {
-    return res.status(401).json({ error: 'Not Authorized' });
-  }
-  // route
-  let url = await route(type, path, auth, req)
+  console.log("hi")
+  let url = await resolve(req.originalUrl, config)
+  console.log(url)
   options = {
     uri: url,
     encoding: null,
