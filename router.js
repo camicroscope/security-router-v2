d0c2a04eec02af6aa53cbad4d9b7801be2a2fb2b
Ryan Birmingham <birm@rbirm.us>
2019-07-02 15:56:10 -0400

    try rjson

--- router.js
+++ router.js
@@ -8,6 +8,7 @@ var jwt = require('jsonwebtoken');
 var proxy = require('http-proxy-middleware');
 const https = require('https')
 var cookieParser = require('cookie-parser')
+const RJSON = require('relaxed-json')
 
 var PUBKEY = process.env.PUBKEY
 var DISABLE_SEC = process.env.DISABLE_SEC || false
@@ -62,7 +63,7 @@ try {
   console.error(err)
 }
 
-let loading_config = JSON.parse(fs.readFileSync("routes.json"));
+let loading_config = RJSON.parse(fs.readFileSync("routes.json"));
 
 const config = loading_config;
 
